56b16d75dca8d9113be8d6457d42dc7d9ae7835a
Nicolas Chamorro <nicolaschamorro@cisde.co>
2020-11-23 10:22:08 -0500

    formulario arreglado

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -4,15 +4,16 @@ $name = $_POST['name'];
 $msg = $_POST['msg'];
 $asunto = $_POST['asunto'];
 
-$to = $_POST['email'];
+$to = 'osmel@maquillaje.biz'; //cosmeticososmel
+
 $subject = 'Mensaje desde pagina web';
 $message = "Nombre: $name"."\nAsunto: $asunto"."\nMensaje: $msg";
 
-$headers = "From: osmel@maquillaje.biz";
+$headers = "From:".$_POST['email'];
+
 
 mail($to, $subject, $message, $headers);
 
-//hay un script que me esta realizando esto por ayax 
 
 
 ?>
